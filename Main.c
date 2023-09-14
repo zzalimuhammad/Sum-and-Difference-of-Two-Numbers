ad8c41303264a004d54da05923df0d972e96ef67
alimuhamm <alimuhammadabuamer@gmail.com>
2023-09-14 05:40:36 +0300

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2;
+    float num3,num4;
+    scanf("%d%d",&num1,&num2);
+    scanf("%f%f",&num3,&num4);
+    printf("%d %d\n",num1+num2,num1-num2);
+    printf("%.1f %.1f",num2+num4,num3-num4);
+}
